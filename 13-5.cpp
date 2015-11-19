f51c8332bc6e2964719364897654a5c2f65213d2
Wamkelekile <whoopielife@gmail.com>
2015-11-19 18:07:42 +0300

    tocheck

--- 13-5.cpp
+++ 13-5.cpp
@@ -12,17 +12,37 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include <stab.h>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 struct Stab {
-  uint32_t n_strx;
-  uint8_t n_type;
-  uint8_t n_other;
-  uint16_t n_desc;
-  uintptr_t n_value;
+  	uint32_t n_strx;
+  	uint8_t n_type;
+  	uint8_t n_other;
+  	uint16_t n_desc;
+  	uintptr_t n_value;
 
 };
 
+struct myStab {
+	string name;
+	int index;
+	uintptr_t start;
+	uintptr_t end;
+	string source;
+
+};
+
+bool comp(const myStab& stab1, const myStab& stab2) {
+	int res = strcmp(stab1.name.c_str(), stab2.name.c_str());
+	if (res < 0) {
+		return 1;
+	} else if (res == 0) {
+		if (stab1.start < stab2.start) return 1;
+		else return 0;
+	} else return 0;
+}
 
 class File {
 public:
@@ -60,82 +80,52 @@ int main(int argc, char *argv[]) {
 	//}
 	File f(argv[1]);
 	if (f.get_fd() == -1) {
-		cerr << "Couldn't open input file" << endl;
-		return 1;
+		return 0;
 	}
 	Elf32_Ehdr header_stat;
 	//int *kmap;
-
 	char *current_char = (char *) mmap(NULL, f.size(), PROT_READ, MAP_SHARED, f.get_fd(), 0);
 	//int filedesk = (int) mmap(NULL, f.size(), PROT_READ, MAP_SHARED, f.get_fd(), 0);
 	int well_red = read(f.get_fd(), &header_stat, 52);
-	// ERRORS
-	if (well_red != 52) {
-		cerr << "Error while reading ELF Header" << endl;
-		return 1;
-	}
-	if (!((header_stat.e_ident[0] == '\x7f') && (header_stat.e_ident[1] == 'E')
-	 	&& (header_stat.e_ident[2] == 'L') && (header_stat.e_ident[3] = 'F'))) {
-		cerr << "Not an ELF file" << endl;
-		return 1;
-	}
-	if ((header_stat.e_ident[4] != 1) ||
-	 ((header_stat.e_ident[5] != 1) && (header_stat.e_ident[5] != 2)) ||
-	(header_stat.e_ident[6] != 1) ) {
-		cerr << "Not supported ELF file" << endl;
-		return 1;
-	}
-
-
 
 	// START pRINT
 
-	if (header_stat.e_type == 0) {
-		printf("TYPE: NONE\n");
-	} else if(header_stat.e_type == 1) {
-		printf("TYPE: REL\n");
-	} else if(header_stat.e_type == 2) {
-		printf("TYPE: EXEC\n");
-	} else if(header_stat.e_type == 3) {
-		printf("TYPE: DYN\n");
-	} else if(header_stat.e_type == 4) {
-		printf("TYPE: CORE\n");
+	if(header_stat.e_type != 2) {
+		return 0;
 	}
+	uint8_t is_debug_info = 0;
 	Elf32_Shdr names_section_header;
 	pread(f.get_fd(), &names_section_header, 40,
 		 header_stat.e_shoff + 40 * header_stat.e_shstrndx);
-	printf("%20s %10s %10s %10s %6s\n", "NAME", "ADDR", "OFFSET", "SIZE", "ALGN");
 	Elf32_Shdr stab_header, stabstr_header;
 	for(int i = 0; i <= header_stat.e_shnum - 1; ++i) {
-		Elf32_Shdr cur_sec_header; 
+		Elf32_Shdr cur_sec_header;
 		pread(f.get_fd(), &cur_sec_header,
 					  40, header_stat.e_shoff + 40 * i);
 		if(!i) continue;
 		string name;
 		name.resize(20);
 	        pread(f.get_fd(),(char*) name.c_str(), 20,
-		 names_section_header.sh_offset + cur_sec_header.sh_name);
+		 	names_section_header.sh_offset + cur_sec_header.sh_name);
 		string stab_str = ".stab";
 		string stabstr_str = ".stabstr";
 		if (strcmp(name.c_str(), stab_str.c_str()) == 0) {
-		  stab_header = cur_sec_header;
-		  cout << "stub" << endl;
+		  	stab_header = cur_sec_header;
+			is_debug_info++;
 		}
 		if (strcmp(name.c_str(), stabstr_str.c_str()) == 0) {
-		  stabstr_header = cur_sec_header;
-		  cout << "stubstr" << endl;
+		  	stabstr_header = cur_sec_header;
+			is_debug_info++;
 		}
-		//printf("%20s 0x%08x %10d %10d 0x%04x\n",
-		//	 name.c_str(), cur_sec_header.sh_addr,
-		//	 cur_sec_header.sh_offset, cur_sec_header.sh_size,
-		//	 cur_sec_header.sh_addralign);
 	}
+	if (is_debug_info != 2) {
+		printf("No debug info\n");
+		return 0;
+	}
+	vector<myStab> all_func;
 	Stab curr_stab;
 	int count_of_blocks = stab_header.sh_size / sizeof(curr_stab);
-	cout << count_of_blocks << endl;
-	//char * read_fun_name = current_char;
-
-	int in_comp = -1;
+	bool in_comp = false;
 	bool is_func = false;
 	bool sline_was = false;
 	string source, true_f_name;
@@ -145,14 +135,23 @@ int main(int argc, char *argv[]) {
 		if (!j) continue;
 	  	pread(f.get_fd(), &curr_stab, sizeof(curr_stab),
 			stab_header.sh_offset + sizeof(curr_stab) * j);
+		//if (in_comp) cout << "WELL" << endl;
 		if (curr_stab.n_type == N_SO) {
 			if (in_comp) { //push func_desc we have f_end and source
-				printf("%s %d 0x%08x 0x%08x %s\n", true_f_name.c_str(), f_index, f_start, curr_stab.n_value, source.c_str());
+				//printf("%s %d 0x%08x 0x%08x %s\n", true_f_name.c_str(), f_index, f_start, curr_stab.n_value, source.c_str());
+				myStab tmp_f;
+				tmp_f.name = true_f_name;
+				tmp_f.index = f_index;
+				tmp_f.start = f_start;
+				tmp_f.end = curr_stab.n_value;
+				tmp_f.source = source;
+				all_func.push_back(tmp_f);
 				sline_was = false;
 			}
-			in_comp *= -1;
+			in_comp  = !in_comp;
+			//is_func = false;
 		}
-		if ((curr_stab.n_type == 0x84)) {
+		if ((curr_stab.n_type == N_SOL) && (is_func) && (!sline_was)) {
 			string source_name;
 			source_name.resize(50);
 			char * source_ptr = (char *) source_name.c_str();
@@ -163,13 +162,19 @@ int main(int argc, char *argv[]) {
 				source_ptr++;
 			}
 			*source_ptr = '\0';
-			cout << "SOURCE" << source_name << endl;
 			source = source_name;
 		}
 		if ((curr_stab.n_type == N_SLINE) && is_func) sline_was = true;
 	  	if (curr_stab.n_type == N_FUN) {
 			if (is_func) { // push f_sescription
-				printf("%s %d 0x%08x 0x%08x %s\n", true_f_name.c_str(), f_index, f_start, curr_stab.n_value, source.c_str());
+				//
+				myStab tmp_f;
+				tmp_f.name = true_f_name;
+				tmp_f.index = f_index;
+				tmp_f.start = f_start;
+				tmp_f.end = curr_stab.n_value;
+				tmp_f.source = source;
+				all_func.push_back(tmp_f);
 				sline_was = false;
 			}
 			is_func = true; // expect N_So or N_func
@@ -179,7 +184,7 @@ int main(int argc, char *argv[]) {
 			fun_name.resize(50);
 			char * name = (char *) fun_name.c_str();
 			char * read_fun_name = current_char + stabstr_header.sh_offset + curr_stab.n_strx;
-			while(*read_fun_name != '\0') {
+			while(*read_fun_name != ':') {
 				*name = *read_fun_name;
 				read_fun_name++;
 				name++;
@@ -193,8 +198,13 @@ int main(int argc, char *argv[]) {
 	    		//cout << "J:"  << fun_name << endl;
 			//printf("%s %d 0x%08x 0x%08x %s\n", fun_name.c_str(), j, curr_start.n_value, 1, "SOURCE");
 	  	}
+		//SORT
 
 	  // name index start end source
+	}
+	sort(all_func.begin(), all_func.end(), comp);
+	for (size_t k = 0; k < all_func.size(); ++k) {
+		printf("%s %d 0x%08x 0x%08x %s\n", all_func[k].name.c_str(), all_func[k].index, all_func[k].start, all_func[k].end, all_func[k].source.c_str());
 	}
 
 
