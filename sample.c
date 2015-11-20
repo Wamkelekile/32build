efb8a025c9460ec6640d1625099d110c6908c6b9
Wamkelekile <whoopielife@gmail.com>
2015-11-20 15:31:40 +0300

    s

---
+++ sample.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "sample.h"
+int val;
+int func1(void)
+{
+    return 0;
+}
+
+int func2(void)
+{
+    return 0;
+}
+
+int func3(void)
+{
+    return 0;
+}
+
+int main()
+{
+    func1();
+    func2();
+    func3();
+    func4();
+    func5();
+    func6();
+    return 0;
+}
